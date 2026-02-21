27a9bbc8ff88b7eeb3dbab6fd7cd04b625e6dde2
slr-09 <ge12090@naver.com>
2026-02-21 21:04:31 +0900

    [#23] refactor: Onboarding Components 폴더 분리

--- Chac/Chac/Source/Presentation/Onboarding/Components/OnboardingFirstPageView.swift
+++ Chac/Chac/Source/Presentation/Onboarding/Components/OnboardingFirstPageView.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 struct OnboardingFirstPageView: View {
     
+    public enum Strings {
+        static let title = "사진을 정리해\n앨범을 생성해주는 착"
+    }
+    
     var body: some View {
         ZStack {
             Image("onboarding_background")
@@ -25,7 +29,7 @@ struct OnboardingFirstPageView: View {
                     .foregroundStyle(ColorPalette.text_btn_01)
                     .padding(.top, 26)
 
-                Text("사진을 정리해\n앨범을 생성해주는 착")
+                Text(Strings.title)
                     .chacFont(.headline_01)
                     .foregroundStyle(ColorPalette.text_01)
                     .multilineTextAlignment(.center)
